aa4694a3564ea9b8f9398c6b8a2ad74c05806d05
4THPC\t-kojima <t-kojima@skullware.net>
2018-05-24 01:35:51 +0900

    fix:  $id to $id.value in app.js.

--- test/app.record.js
+++ test/app.record.js
@@ -35,6 +35,23 @@ describe('app.record', () => {
       const record = kintone.app.record.get();
       assert.equal(record.record['数値'].value, '99');
     });
+
+    it('kintone.app.record.getId()が取得できること', async () => {
+      kintone.events.on(method, event => event);
+      await kintone.events.do(method, { recordId: '3' });
+      assert.equal(kintone.app.record.getId(), '3');
+    });
+
+    it('kintone.app.record.set()で値が反映されること', async () => {
+      kintone.events.on(method, event => event);
+      await kintone.events.do(method);
+
+      const record = kintone.app.record.get();
+      record.record['数値'].value = '77';
+      kintone.app.record.set(record);
+
+      assert.equal(record.record['数値'].value, '77');
+    });
   });
 
   describe('.kinmockディレクトリが無い場合', () => {
@@ -55,6 +72,12 @@ describe('app.record', () => {
         kintone.events.on(method, event => event);
         await kintone.events.do(method);
         assert.isNull(kintone.app.record.get());
+      });
+
+      it('kintone.app.record.getId()がnullであること', async () => {
+        kintone.events.on(method, event => event);
+        await kintone.events.do(method, { recordId: '3' });
+        assert.isNull(kintone.app.record.getId());
       });
     });
   });
