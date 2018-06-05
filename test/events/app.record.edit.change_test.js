a26729c3b2650334ef303c6ef0dcaead902ca7d7
t-kojima <t-kojima@skullware.net>
2018-06-06 00:54:23 +0900

    add: app.record.edit.change_test.js

---
+++ test/events/app.record.edit.change_test.js
@@ -0,0 +1,31 @@
+/* eslint-disable no-undef, no-param-reassign */
+require('../../lib');
+const { assert } = require('chai');
+
+const getActual = async (id) => {
+  const method = 'app.record.index.edit.show';
+  kintone.events.on(method, event => event);
+  const event = await kintone.events.do(method, { recordId: id });
+  kintone.events.off(method);
+  return event.record;
+};
+
+describe('app.record.edit.change.<フィールド>', () => {
+  const method = 'app.record.edit.change.数値';
+  afterEach(() => {
+    kintone.events.off(method);
+    kintone.loadDefault();
+  });
+
+  describe('returnしない場合', () => {
+    it('トリガーとなった値の変更がキャンセルされること', async () => {
+      kintone.events.on(method, (event) => {
+        event.record['文字列__複数行_'].value = 'DUMMY2';
+      });
+      await kintone.events.do(method, { recordId: '1', value: '999' });
+
+      const actual = await getActual('1');
+      assert.equal(actual.数値.value, '99');
+    });
+  });
+});
