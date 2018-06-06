71f96d039d4b31ee48c15fce92ca02845eb387a1
t-kojima <t-kojima@hokuichi-e.com>
2018-06-06 11:04:24 +0900

    feat: update getQueryCondition

--- test/index_test.js
+++ test/index_test.js
@@ -12,4 +12,34 @@ describe('kintone関数', () => {
     const actual = kintone.getLoginUser();
     assert.equal(actual.name, 'no-name');
   });
+
+  describe('getQueryCondition', () => {
+    describe('レコード一覧画面', () => {
+      const method = 'app.record.index.show';
+      afterEach(() => kintone.events.off(method));
+
+      it('カスタムビューの絞り込み情報を取得できること', async () => {
+        await kintone.events.do(method, { viewId: '5519903' });
+        const actual = kintone.app.getQueryCondition();
+        assert.equal(actual, '数値 >= "1000" and 文字列__1行_ not like "NGWORD"');
+      });
+
+      it('デフォルトビューの場合空文字が返ること', async () => {
+        await kintone.events.do(method);
+        const actual = kintone.app.getQueryCondition();
+        assert.equal(actual, '');
+      });
+    });
+
+    describe('レコード詳細画面', () => {
+      const method = 'app.record.detail.show';
+      afterEach(() => kintone.events.off(method));
+
+      it('nullが返ること', async () => {
+        await kintone.events.do(method, { recordId: '1', viewId: '5519903' });
+        const actual = kintone.app.getQueryCondition();
+        assert.isNull(actual);
+      });
+    });
+  });
 });
